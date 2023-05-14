ba0c1f2bf57dc074357d3ba76434a303afa4ecfc
Samanuay <200303105038@paruluniversity.ac.in>
2023-05-14 11:42:57 +0530

    v5

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -18,7 +18,7 @@ const transporter = nodemailer.createTransport({
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "server/uploads/");
+        cb(null, "uploads/");
     },
     filename: (req, file, cb) => {
         cb(null, Date.now()+file.originalname );
